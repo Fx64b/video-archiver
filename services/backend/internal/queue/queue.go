9ae8f6f21e0b585f313dd5344d707a984eaeaff3
Fx64b <90456762+Fx64b@users.noreply.github.com>
2024-10-27 12:16:22 +0100

    feat: improve queue and stream logs

--- services/backend/internal/queue/queue.go
+++ services/backend/internal/queue/queue.go
@@ -1,10 +1,14 @@
 package queue
 
 import (
+	"bufio"
 	"fmt"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
+	"io"
 	"os"
 	"os/exec"
+	"regexp"
+	"strconv"
 	"video-archiver/models"
 )
 
@@ -19,11 +23,11 @@ func StartQueueWorker() {
 }
 
 func processJob(job models.DownloadJob) {
-	logrus.Infof("Starting download for job ID: %s, URL: %s", job.ID, job.URL)
+	log.Infof("Starting download for job ID: %s, URL: %s", job.ID, job.URL)
 
 	downloadPath := os.Getenv("DOWNLOAD_PATH")
 	if downloadPath == "" {
-		downloadPath = "./downloads" // Default to relative path in the container
+		downloadPath = "./downloads"
 	}
 
 	outputPath := fmt.Sprintf("%s/%%(uploader)s/%%(playlist|title)s/%%(title)s.%%(ext)s", downloadPath)
@@ -40,11 +44,61 @@ func processJob(job models.DownloadJob) {
 		job.URL,
 	)
 
-	err := cmd.Run()
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
 
-	if err != nil {
-		logrus.Errorf("Failed to download job ID %s: %v", job.ID, err)
-	} else {
-		logrus.Infof("Successfully downloaded job ID: %s", job.ID)
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		log.Errorf("Failed to start yt-dlp for job ID %s: %v", job.ID, err)
+		return
 	}
+
+	// Track progress in a separate goroutine
+	go trackProgress(stdout, job.ID)
+	go trackProgress(stderr, job.ID)
+
+	// Wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		log.Errorf("Failed to download job ID %s: %v", job.ID, err)
+	} else {
+		log.Infof("Successfully downloaded job ID: %s", job.ID)
+	}
+}
+
+func trackProgress(pipe io.Reader, jobID string) {
+	scanner := bufio.NewScanner(pipe)
+	itemRegex := regexp.MustCompile(`\[download\] Downloading item (\d+) of (\d+)`)
+
+	var totalItems, currentItem int
+	var progress float64
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if match := itemRegex.FindStringSubmatch(line); match != nil {
+			currentItem = parseToInt(match[1])
+			totalItems = parseToInt(match[2])
+
+			progress = (float64(currentItem) / float64(totalItems)) * 100
+
+			//log.Infof("Job ID %s: Downloading item %d of %d - %.2f%%", jobID, currentItem, totalItems, progress)
+			fmt.Printf("Job ID %s: Downloading item %d of %d - %.2f%%\n", jobID, currentItem, totalItems, progress)
+		}
+	}
+}
+
+func parseToInt(s string) int {
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return val
+}
+
+func parseToFloat(s string) float64 {
+	val, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0.0
+	}
+	return val
 }
