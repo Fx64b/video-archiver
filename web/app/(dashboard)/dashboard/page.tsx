56a080c2390301c6ca8b60c88f95dd96a15b1f68
Fx64b <90456762+Fx64b@users.noreply.github.com>
2024-12-13 23:10:51 +0100

    feat(web/dashboard): add placeholder pages

---
+++ web/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Home() {
+    return (
+        <div
+            className="flex flex-col w-full min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col w-full">
+                <p>Dashboard Works</p>
+            </main>
+        </div>
+    );
+}
+
